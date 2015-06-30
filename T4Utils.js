08cb59f2d5b90147e635aea276a2d9fff4e875ab
Ben Margevicius <bdm4@case.edu>
2015-06-30 16:22:12 -0400

    Added Untested V.11 functions.

--- T4Utils.js
+++ T4Utils.js
@@ -1,11 +1,16 @@
 /* 
 	Utility Javascript for T4 Javascript Content Processor
    	Ben Margevicius; bdm4@case.edu
-	Version 0.1x 6/24/2015
+	Version 0.11 6/30/2015
    
 	Github source: https://github.com/CaseWesternReserveUniversity/T4Utils/
     6/24/2015 - Initial
-
+	6/30/2015 - Added stuff from T4's javascript util (https://community.terminalfour.com/forum/index.php?topic=426.0)
+				Added utils.version;
+				Added utils.siteManager namespace 
+				Added utils.siteManager.version, and utils.siteManager.buildDetails;
+				Added utils.brokerUtils namespace
+				Added utils.brokerUtils.processT4Tags
 	Usage:
 	1) Add a content type, modify the content layout, paste this at the top of your layout. 
 	2) Your code will go below the T4Utils Object
@@ -23,12 +28,35 @@ var T4Utils = (function (utils) {
   	/* T4Utils.write 
        Writes some text between some paragraph tags
     */
+	//version of this utility class
+	utils.version = '6/30/2015 v0.11';	
+	
     utils.write = function(text)
     {
       document.write("<p>" + text + "</p>");
-    }
+    };
     
-    /* getSectionInfo namespace gets 
+	/* 
+		sitemanager namespace gets information about the sitemanager 	
+	*/
+	utils.siteManager = utils.siteManager || {};
+	//write the version of T4 you are working with.
+	utils.siteManager.version = com.terminalfour.sitemanager.SiteManagerVersion.version;	
+	utils.siteManager.buildDetails = com.terminalfour.sitemanager.SiteManagerVersion.buildDetails;
+	
+	/* 
+		BrokerUtils NameSpace does something. I dunno yet.. I'll have to read the API :) 
+	
+	*/
+	utils.brokerUtils = utils.brokerUtils || {};
+	utils.brokerUtils.processT4Tag = function (t4Tag) {
+		var myContent = content || null; 
+		return com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, myContent, language, isPreview, t4Tag); 
+	};
+	
+    /* 
+	
+		getSectionInfo namespace gets 
        information about a section. duh.       
     */
     utils.getSectionInfo = utils.getSectionInfo || {};
