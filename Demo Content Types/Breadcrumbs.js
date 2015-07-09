dd7793123b3810d0412f5126a27d1da656568acd
Ben Margevicius <bdm4@case.edu>
2015-07-09 12:49:55 -0400

    Updated the demo content. It's more of a backup of what I have..

--- Demo Content Types/Breadcrumbs.js
+++ Demo Content Types/Breadcrumbs.js
@@ -1,16 +1,90 @@
+/* 
+	Utility Javascript for T4 Javascript Content Processor
+   	Ben Margevicius; bdm4@case.edu
+	Version 0.13.2
+   
+	Github source: https://github.com/CaseWesternReserveUniversity/T4Utils/	
+*/
+ 
 importClass(com.terminalfour.publish.PathBuilder); //import the pathbuilder class
 
 //IIFE for t4Utils. NOTE you can't use the window namespace for window.ns = window.ns || {} 
-var T4Utils = (function (utils) {
-  	/* T4Utils.write 
-       Writes some text between some paragraph tags
-    */   
+var T4Utils = (function (utils) {  	
+	utils.version = 'v0.13.2';	
+	
+	/* Console utils for debugging. Don't leave these in your layouts.. 
+		T4Utils.console.log("log message");
+		T4Utils.console.warn("warning");
+		T4Utils.console.error("error message");
+		T4Utils.console('<console method>', '<message>');
+	*/
+	utils.console = function(consoleMethod, textOrObj) {		
+		if(typeof textOrObj === "string")			
+			document.write("<script>console." + consoleMethod + "('" + textOrObj + "');</script>\n");				
+	};
+	utils.console.log = function(textOrObj) {		
+		if(typeof textOrObj === "string")			
+			document.write("<script>console.log('" + textOrObj + "');</script>\n");				
+	};
+	utils.console.warn = function(textOrObj) {		
+		if(typeof textOrObj === "string")			
+			document.write("<script>console.warn('" + textOrObj + "');</script>\n");				
+	};
+	utils.console.error = function(textOrObj) {		
+		if(typeof textOrObj === "string")			
+			document.write("<script>console.error('" + textOrObj + "');</script>\n");				
+	};
+	
     utils.write = function(text)
     {
-      document.write("<p>" + text + "</p>");
-    }
+      document.write("<p>" + text + "</p>\n");
+    };
     
-    /* getSectionInfo namespace gets 
+	/* 
+		sitemanager namespace gets information about the sitemanager 	
+	*/
+	utils.siteManager = utils.siteManager || {};
+	//write the version of T4 you are working with.
+	utils.siteManager.version = com.terminalfour.sitemanager.SiteManagerVersion.version;	
+	utils.siteManager.buildDetails = com.terminalfour.sitemanager.SiteManagerVersion.buildDetails;
+	utils.siteManager.javaVersion = java.lang.System.getProperty("java.version");
+	/* 
+		BrokerUtils NameSpace does something. I dunno yet.. I'll have to read the API :) 
+	
+	*/
+	utils.brokerUtils = utils.brokerUtils || {};
+	utils.brokerUtils.processT4Tag = function (t4Tag) {
+		var myContent = content || null; 
+		return com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, myContent, language, isPreview, t4Tag); 
+	};
+	/*
+	  contentInfo namespace gets information about content	
+		note: in layouts content will be null.
+	*/
+	utils.elementInfo = utils.elementInfo || {};
+	utils.elementInfo.getElements = function() {
+		var c = content || null;
+		if(c !== null)
+			c.getElements();	
+	}
+	utils.elementInfo.getElementValue = function(elementName) 
+	{
+		var c = content || null; 
+		if(c !== null)
+			return c.get(elementName).publish();		
+	}
+	//This may not work. 
+	utils.elementInfo.getElementName = function(element) 
+	{
+		var c = content || null; 
+		if(c !== null)
+			return c.get(element).getName();		
+	}
+		
+	
+    /* 
+	
+		getSectionInfo namespace gets 
        information about a section. duh.       
     */
     utils.getSectionInfo = utils.getSectionInfo || {};
@@ -82,10 +156,7 @@ HTML TO WRiTE
 */
 
 function writeBreadCrumbs(ancestors) 
-{
-  	document.write("<div class=\"row\">\n");
-	document.write("	<h1 class=\"header-text\">Breadcrumb Demo<\/h1>\n");
-	document.write("<\/div>\n");
+{  
     document.write("<div class=\"row\">\n");
     document.write("	<div class=\"col-sm-12\">\n");
     document.write("		<ol class=\"breadcrumb\">\n");
--- Demo Content Types/Navbar Pills.js
+++ Demo Content Types/Navbar Pills.js
@@ -1,11 +1,19 @@
 /* 
+	Left Nav using bootstrap pills 
+    By Ben Margevicius bdm4@case.edu
+    7/8/2015
+    V0.1 initial.
+    v0.2 Added Element title 
+    Right now the left navbar will only show the siblings of the parent.	
+*/
+
+/* 
 	Utility Javascript for T4 Javascript Content Processor
    	Ben Margevicius; bdm4@case.edu
 	Version 0.13.2
    
 	Github source: https://github.com/CaseWesternReserveUniversity/T4Utils/	
 */
- 
 importClass(com.terminalfour.publish.PathBuilder); //import the pathbuilder class
 
 //IIFE for t4Utils. NOTE you can't use the window namespace for window.ns = window.ns || {} 
@@ -146,12 +154,11 @@ var T4Utils = (function (utils) {
     return utils; //return out of the module 
 })(T4Utils || {});
 
-
 function getSectionChildrenAndGrandChildren(t4section) {  
   var _mysections = [];
   var _mysection = function() {
-      this.name = "";
-      this.link = "";
+      this.name = '';
+      this.link = '';
       this.children = [];
   };
   
@@ -186,113 +193,62 @@ function getSectionChildrenAndGrandChildren(t4section) {
   return _mysections;
 }
 
-var mysections = getSectionChildrenAndGrandChildren(section); 
-var rootpath = T4Utils.getSectionInfo.getRootPath(section.getParent()); //get the path to root for the first menu item, I don't want to include this page so get it's parent as the first menu item
-if(mysections.length === 0) //there are no children in the navbar. so lets see if we can add some siblings instead.
-{
-  /*
-  if('true' == T4Utils.elementInfo.getElementValue('show siblings')) {
-  	var parentsection = section.getParent();
-    var siblings = parentsection.getChildren();
-  }*/
-  mysections = getSectionChildrenAndGrandChildren(section.getParent()); //gets the siblings and thier children of this. 
+var mysection = section.getParent(); //get the parent section. 
+var sectionTitle = T4Utils.getSectionInfo.sectionTitle(section); //current section title
+
+if(sectionTitle == 'LColumn' || sectionTitle == 'RColumn') {  //If the left nav is nested in a hidden section then we have to traverse back to the grandparent.     
+  	sectionTitle = T4Utils.getSectionInfo.sectionTitle(mysection); //current section title; //change the section title to the name of the grandparent..
+  	mysection = mysection.getParent(); //get the grantparent.
 }
+var siblings = getSectionChildrenAndGrandChildren(mysection); //gets the siblings and thier children of this.
 
 
-writeNAVBar(rootpath, mysections);
-
-
-/*Write BootStrap NAVBar 
-Rules:
- 	1) No GreatGrand Children menus/ sub-sub menus/ Dropdowns will NOT have other dropdowns. This is stamped and sealed by twitter bootstrap. Thou shalt not whine. 
-    2) If a section has grandchildren, they are listed in a drop down list (DDL). The root of the DDL is not a navigatable anchor tag <a href="#">Home</a>. If it is kiss your responsive mobile menus goodbye. 
-    3) If a section does not have grandchildren it is a navigatable anchor tag.
-
-params: 
-ancestors is an array of sections to root    
-children is an object with the name, link of a child and an array of names and links to it's grandchildren.
-*/
-function writeNAVBar(ancestors, sections)
+writeLeftNAV(siblings, sectionTitle);
+function writeLeftNAV(sibs, currSectionTitle)
 {  
-document.write("<nav class=\"navbar navbar-default\" role=\"navigation\">");
-document.write("  	<div class=\"navbar-header collapsed\" data-toggle=\"collapse\" data-target=\"#mobile-navbar-collapse\">");
-document.write("      <a id=\"mobile-nav-header\" class=\"visible-xs text-center \" href=\"#\"><span class=\"glyphicon glyphicon-list-alt \" style=\"padding-right:3px;\"><\/span> Navigation & Search<\/a>");
-document.write("    <\/div>");
-document.write("    <div class=\"collapse navbar-collapse\" id=\"mobile-navbar-collapse\">");
-document.write("       <form class=\"navbar-form navbar-left visible-xs\" role=\"search\" action=\"\/\/www.google.com\/cse\"  target=\"_parent\" name=\"searchForm\">");
-document.write("       <input type=\"hidden\" name=\"cx\" value=\"013802920388319893419:7wtcwv_ppd8\" \/>");
-document.write("       <input type=\"hidden\" name=\"ie\" value=\"UTF-8\" \/>");
-document.write("        ");
-document.write("         <div class=\"input-group\">");
-document.write("			<label for=\"mobile-search-text\" class=\"sr-only\">Search<\/label>");
-document.write("      		<input id=\"mobile-search-text\" size=\"30\" name=\"q\" type=\"text\" class=\"form-control\" placeholder=\"Google Search...\" onfocus=\"if(!this._haschanged){this.value=''};this._haschanged=true;\">");
-document.write("      		<span class=\"input-group-btn\">");
-document.write("        		<button class=\"btn btn-default\" type=\"submit\" name=\"sa\">");
-document.write("                	<!-- Search -->");
-document.write("                	<span class=\"glyphicon glyphicon-search\"><\/span>");
-document.write("					<span class=\"sr-only\">search button<\/span>");
-document.write("                <\/button>");
-document.write("      		<\/span>");
-document.write("    	<\/div><!-- \/input-group -->");
-document.write("      <\/form>");
-
-  
-  
-//Start Writing the NAV Lists  
-document.write("      <ul class=\"nav navbar-nav\">\n");
-
-//First lets write our first menu item
-document.write("<li class=\"dropdown\"><a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-expanded=\"false\">Home<span class=\"caret\"></span></a>\n");
-document.write("<ul class=\"dropdown-menu\" role=\"menu\">\n");  
-for(ancestor = 0; ancestor < ancestors.length; ancestor++)
-{  
-	document.write("<li><a class=\"smaller-menu\" href=\"" +  T4Utils.getSectionInfo.sectionLink(ancestors[ancestor]) + "\">" + T4Utils.getSectionInfo.sectionTitle(ancestors[ancestor]) + "<\/a><\/li>\n");
-} 
-document.write("<\/ul><\/li>\n");  
-//end the ancestors menu bit
-
-//start writing children and grandbabies.
-for(i = 0; i < sections.length; i++)
-{  
-  var children = sections[i].children; 
-  if(children.length > 0) //if there is grand children 
-  {
-  	document.write("<li class=\"dropdown\"><a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-expanded=\"false\">" + sections[i].name + "<span class=\"caret\"></span></a>");
-    document.write("<ul class=\"dropdown-menu\" role=\"menu\">");
-    document.write("<li><a class=\"smaller-menu bg-info\" href=\"" + sections[i].link+ "\"><span class=\"glyphicon glyphicon-home glyphicon-pad-right glyphicon-menu-icon\"><\/span>" + sections[i].name + "<\/a><\/li>");    
-    for(k = 0; k < children.length; k++)
+  	try
     {
-    	document.write("<li><a class=\"smaller-menu\"  href=\"" + T4Utils.getSectionInfo.sectionLink(children[k]) + "\">" + T4Utils.getSectionInfo.sectionTitle(children[k]) + "<\/a><\/li>");
-    }    
-    document.write("<\/ul><\/li>");
-  }
-  else
-  {	
-	//child with no grand children.    
-    document.write("<li><a href=\"" + sections[i].link+ "\">" + sections[i].name + "<\/a><\/li>");
-  } 
-}  
-document.write("      <\/ul>"); //close ul class= NAV LEFT
-
-//Write any right side navbar links 
-var addrightlinks = ('true' == content.get('Add sitelinks').publish()); //convert text to boolean   
-if(addrightlinks)
-{
-  document.write("<ul class=\"nav navbar-nav navbar-right\">");
-  document.write("        <li><a style=\"font-weight:200;color: #993CF3;\" href=\"#\">Link<\/a><\/li>");
-  document.write("        <li class=\"dropdown\">");
-  document.write("          <a style=\"font-weight:200;color: #993CF3;\" href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">Dropdown <span class=\"caret\"><\/span><\/a>");
-  document.write("          <ul class=\"dropdown-menu\">");
-  document.write("            <li><a style=\"font-weight:200;color: #993CF3;\" href=\"#\">Action<\/a><\/li>");
-  document.write("            <li><a style=\"font-weight:200;color: #993CF3;\" href=\"#\">Another action<\/a><\/li>");
-  document.write("            <li><a style=\"font-weight:200;color: #993CF3;\" href=\"#\">Something else here<\/a><\/li>");
-  document.write("            <li role=\"separator\" class=\"divider\"><\/li>");
-  document.write("            <li><a style=\"font-weight:200;color: #993CF3;\" href=\"#\">Separated link<\/a><\/li>");
-  document.write("          <\/ul>");
-  document.write("        <\/li>");
-  document.write("      <\/ul>");  
-}  
-  
-document.write("   <\/div>"); //close <div class=collapse navbar-collapse\
-document.write("<\/nav>"); //close nav element
+      var title = T4Utils.elementInfo.getElementValue('Title');
+      if(title !== null)
+      {         
+      	document.write("<h3>" + title + "<\/h3>\n");
+      }
+      
+      document.write("<ul class=\"nav nav-pills\">\n");  
+    
+      for(sibling = 0; sibling < sibs.length; sibling++)
+      {	            	
+          var n = String(sibs[sibling].name);
+          var s = String(currSectionTitle);
+          
+          if(n !== s)
+          {
+              document.write("<li role=\"presentation\"><a href=\""+ sibs[sibling].link +"\">" + n + "<\/a><\/li>\n");
+          }
+          else          
+          {
+              T4Utils.console.log("adding active");          
+              document.write("<li role=\"presentation\" class=\"active\"><a href=\""+ sibs[sibling].link +"\">" + n + "<\/a><\/li>\n");
+          }
+      }    
+      document.write("<\/ul>\n");
+    }
+  	catch(err)
+    {
+      T4Utils.console.error("Failed to write the left nav bar with error: " + err.message);
+    }
 }
+
+
+/*
+<h3>2 column menu</h3>
+<ul class="nav nav-pills">
+	<li role="presentation" class="active"><a href="#2-column-aside">2 Column (w/aside)</a> </li>
+	<li role="presentation"><a href="#2-column-staff">2 Column (staff)</a></li>
+	<li role="presentation"><a href="#2-column-video">2 Column (w/video)</a></li>
+	<li role="presentation"><a href="#2-column-banner">2 Column (w/banner)</a></li>
+	<li role="presentation"><a href="#2-column-thumbs">2 Column (w/thumbs)</a></li>		 
+</ul>
+*/
+T4Utils.console.log("left nav working");
+
