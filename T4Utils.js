9739ddf50d5cece4e7462ef5e2afa299db2836f4
Ben Margevicius <bdm4@case.edu>
2015-09-18 12:30:49 -0400

    Revert "Nightky"
    This reverts commit c69494225d3bcb5e9f8922c0afd6670b209cf2e1.

--- T4Utils.js
+++ T4Utils.js
@@ -16,7 +16,6 @@
 	7/20/2015	Added utils.getSectionInfo.getChildren(section, isHiddenInNAV)  
 	8/13/2015	Added utils.getSectionInfo.getLevel(section)
 				Added utils.toString(obj) 
-	9/17/2015	Added utils.elementInfo.getElementID(element) Returns the id of an element.
 	
 	Usage:
 	1) Add a content type, modify the content layout, paste this at the top of your layout. 
@@ -44,7 +43,7 @@
 /* 
 	Utility Javascript for T4 Javascript Content Processor
    	Ben Margevicius; bdm4@case.edu
-	Version 0.16 9/17/15
+	Version 0.15.1 8/13/15
    
 	Github source: https://github.com/CaseWesternReserveUniversity/T4Utils/	
 */
@@ -80,7 +79,7 @@ importClass(com.terminalfour.publish.PathBuilder); //import the pathbuilder clas
 
 //IIFE for t4Utils. NOTE you can't use the window namespace for window.ns = window.ns || {} 
 var T4Utils = (function (utils) {  	
-	utils.version = 'v0.16';	
+	utils.version = 'v0.15.1';	
 	
 	/* Console utils for debugging. Don't leave these in your layouts.. 
 		T4Utils.console.log("log message");
@@ -140,7 +139,7 @@ var T4Utils = (function (utils) {
 	  contentInfo namespace gets information about content	
 		note: in layouts content will be null.
 	*/
-	utils.elementInfo = utils.elementInfo || {};	
+	utils.elementInfo = utils.elementInfo || {};
 	utils.elementInfo.getElements = function() {
 		var c = content || null;
 		if(c !== null)
@@ -151,18 +150,13 @@ var T4Utils = (function (utils) {
 		var c = content || null; 
 		if(c !== null)
 			return c.get(elementName).publish();		
-	}	
+	}
+	//This may not work. 
 	utils.elementInfo.getElementName = function(element) 
 	{
 		var c = content || null; 
 		if(c !== null)
 			return c.get(element).getName();		
-	}
-	utils.elementInfo.getElementID = function(element) 
-	{
-		var c = content || null; 
-		if(c !== null)
-			return c.get(element).getID();		
 	}
 		
 	
