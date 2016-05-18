51da2a9d422c5ee4104bd14aa31951fb8d64d61f
Ben Margevicius <bdm4@case.edu>
2016-05-18 11:50:16 -0400

    added more robust toHash function, Untested in T4 ATM :large_orange_diamond:

--- components/security.js
+++ components/security.js
@@ -22,7 +22,7 @@ T4Utils.security = T4Utils.security || {};
 T4Utils.security.toSHA256 = function(plainText) {	
 	/* jshint bitwise: false */
 	importPackage(java.security);
-
+	
 	var hash;
 	try
 	{	
@@ -41,4 +41,35 @@ T4Utils.security.toSHA256 = function(plainText) {
 		document.write(e);
 	}
 	return hash;
+};
+
+/**
+*	Hashes a plaintext string into a SHA-256 Hex Encoded String
+*	@function toHash
+* 	@memberof T4Utils.security
+*	@param {string} hashAlgorithm - The hash algorithm you want to use. Valid algorithms are 'MD5', 'SHA-256', 'SHA-384', 'SHA-512'
+*	@param {string} plainText - Plain text value to be hashed
+*	@return {string} A string value of the hash
+*/
+T4Utils.security.toHash = function(hashAlgorithm, plainText) {
+	/* global javax, DatatypeConverter */
+	importPackage(java.security);
+	importClass(javax.xml.bind.DatatypeConverter);
+	/* Available algorithms
+		MD2	The MD2 message digest algorithm as defined in RFC 1319.
+		MD5	The MD5 message digest algorithm as defined in RFC 1321.
+		SHA-1
+		SHA-256
+		SHA-384
+		SHA-512 
+	*/	
+	try
+	{
+		var hash = DatatypeConverter.printHexBinary(MessageDigest.getInstance(hashAlgorithm).digest(plainText.getBytes("UTF-8")));
+		return hash;
+	}
+	catch(err)
+	{
+		document.write(err.message);
+	}
 };
